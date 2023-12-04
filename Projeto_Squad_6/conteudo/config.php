644cb28927f4d7261d9e09af868fd4a48f18b02a
Bruno Cavalcanti <145057314+Brunno-Cavalcanti@users.noreply.github.com>
2023-12-04 15:43:21 -0300

    Add files via upload
    Commit final do projeto.

---
+++ Projeto_Squad_6/conteudo/config.php
@@ -0,0 +1,11 @@
+<?php
+   $dbHost = 'aws.connect.psdb.cloud';
+   $dbUsername = '6stt4f3qx99q5bfhoerb';
+   $dbPassword = 'abc123';
+   $dbName = 'rec_tec';
+
+   $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+   
+?>
+
+  
